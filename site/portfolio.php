d8e79d3dd80d85a7136a743054402bb183c889e5
Vinicius01br <viniciuslima1809@hotmail.com>
2024-05-21 17:26:37 -0300

    php 21/05

--- site/portfolio.php
+++ site/portfolio.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfólio Site</title>
-</head>
-<body>
-    <header>
-    <h1>Nossa Equipe</h1>
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="equipe.php">Equipe</a>
-        <a href="portfolio.php">Portfólio</a>
-    </nav>
-    </header>
-    <main>
-        <article>
+<?php include "cabecalho.php";?>
             <h2>Nossos Trabalhos</h2>
             <details>
                 <summary>Game</summary>
@@ -26,13 +9,4 @@
             <summary>Web Site</summary>
             <p>Lorem ipsiu color dolor sit amet</p>
             </details> 
-        </article>
-
-    </main>
-    <footer>
-        <h2>Site criado como exemplo</h2>
-
-    </footer>
-    
-</body>
-</html>
+        <?php include "rodape.php";?>
